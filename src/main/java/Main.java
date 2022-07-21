59d4c3f79f7533e4a5603d757f72c3cf02eae297
nmescv <mescvnv@gmail.com>
2022-07-21 20:28:25 +0300

    commiiint asddq

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("COMMIT - 1");
         System.out.println("COMMIT - 4");
+        System.out.println("COMMIT - 5");
     }
 }
